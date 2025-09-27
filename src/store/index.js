5a0b8262de355958767760195ba981953c40ee84
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-09-27 22:15:20 +0300

    create counterSlice

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,25 @@ import { createSlice, configureStore } from '@reduxjs/toolkit';
 const initialState = { counter: 0, showCounter: true };
 
 
-createSlice({
+const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers:{
-    increment(){},
-    decrement(){},
-    increase(){},
-    toggleCounter(){}
+    increment(state){
+      state.counter++
+    },
+    decrement(state){
+      state.counter--
+    },
+    increase(state, action){
+       state.counter = state.counter + action.amount
+    },
+    toggleCounter(state){
+      state.showCounter= !state.showCounter
+    }
   }
+})
+
+const store = configureStore({
+  reducer: counterSlice.reducer
 })
